ec7ed6d1b9ab1660e8acd203e077f3c2b68c3000
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-12-17 03:44:37 +0100

    formatting

--- tools/clang-formatAll.sh
+++ tools/clang-formatAll.sh
@@ -2,8 +2,8 @@
 # format all .cpp|.hpp files in the repository
 
 export TOOLS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export REPO_DIR =$TOOLS_DIR/../
- 
+export REPO_DIR="$TOOLS_DIR/../"
+
 find $REPO_DIR/include -type f \( -name "*.hpp" -or  -name "*.cpp" \) | xargs clang-format -i 
 find $REPO_DIR/src -type f \( -name "*.hpp" -or  -name "*.cpp" \) | xargs clang-format -i 
 find $REPO_DIR/examples -type f \( -name "*.hpp" -or  -name "*.cpp" \)  | xargs clang-format -i 
