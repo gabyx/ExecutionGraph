e9d5d8423c87477bc3d87dccfb2a789850db94f9
Simon Spoerri <simon.spoerri@gmail.com>
2018-02-10 10:13:53 +0100

    Move binary HTTP trial to it's own class and use angular's HttpClient

--- gui/executionGraphGui/client/src/app/services/DummyExecutionService.ts
+++ gui/executionGraphGui/client/src/app/services/DummyExecutionService.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ExecutionService } from './ExecutionService';
 
-import { binaryXHRRequest, RequestResult } from '../common/BinaryXHRRequest';
-import * as msgpack from 'msgpack-lite';
-
 @Injectable()
 export class DummyExecutionService extends ExecutionService {
 
@@ -13,25 +10,6 @@ export class DummyExecutionService extends ExecutionService {
 
     public execute(): Promise<void> {
         console.log(`[DummyExecutionService] execute()`);
-
-        var message = msgpack.encode({
-            "requestId": "addNode",
-            "payload": {
-                "graphId": 12,
-                "args": [1, 2, 3]
-            }
-        }) as Uint8Array;
-
-        return new Promise((resolve, reject) => {
-            binaryXHRRequest('post', "backend://executionGraph/addNode", undefined , message).then(
-                (requestResult: RequestResult) => {
-                    console.log(`[DummyExecutionService] binaryXHRRequest() success: ${requestResult.statusText}`);
-                    resolve("`[DummyExecutionService] binaryXHRRequest() success");
-                },
-                (requestResult: RequestResult) => {
-                    console.log(`[DummyExecutionService] binaryXHRRequest() failed: ${requestResult.statusText}`);
-                    reject("[DummyExecutionService] binaryXHRRequest() failed");
-                });
-        })
+        return Promise.resolve();
     }
 }
