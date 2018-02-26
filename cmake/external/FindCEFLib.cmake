678d83d1af0100ae01ee22466184f4e395db3a2d
Gabriel Nuetzi <gnuetzi@gmail.com>
2018-02-26 23:19:21 +0100

    Install dSYM for CEF for Debugging (symlinking)

--- cmake/external/FindCEFLib.cmake
+++ cmake/external/FindCEFLib.cmake
@@ -2,14 +2,10 @@
 if(NOT EXISTS ${CEF_ROOT})
 
     # Determine the platform.
+    set(CEF_DSYM_URL "")
     if("${CMAKE_SYSTEM_NAME}" STREQUAL "Darwin")
-
-        if(${ExecutionGraph_USE_CEF_DEBUG})
-            set(CEF_URL "http://opensource.spotify.com/cefbuilds/cef_binary_3.3282.1741.gcd94615_macosx64_debug_symbols.tar.bz2")
-        else()
-            set(CEF_URL "http://opensource.spotify.com/cefbuilds/cef_binary_3.3282.1733.g9091548_macosx64.tar.bz2")
-        endif()
-
+        set(CEF_DSYM_URL "http://opensource.spotify.com/cefbuilds/cef_binary_3.3282.1741.gcd94615_macosx64_debug_symbols.tar.bz2")
+        set(CEF_URL "http://opensource.spotify.com/cefbuilds/cef_binary_3.3282.1733.g9091548_macosx64.tar.bz2")
     elseif("${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
         set(CEF_URL "http://opensource.spotify.com/cefbuilds/cef_binary_3.3282.1733.g9091548_linux64.tar.bz2")
     elseif("${CMAKE_SYSTEM_NAME}" STREQUAL "Windows")
@@ -30,6 +26,14 @@ if(NOT EXISTS ${CEF_ROOT})
 
     set(CEF_ROOT "${cefbinaries_SOURCE_DIR}" CACHE PATH "CEF director path" FORCE)
 
+    # Download the dSYM file for Debugging.
+    if(${ExecutionGraph_USE_CEF_DSYM_FILE} AND (NOT ${CEF_DSYM_URL}  STREQUAL ""))
+        download_project(PROJ               "cefbinaries-dsymfile"
+                        PREFIX              "${ExecutionGraph_EXTERNAL_BUILD_DIR}/cefbinaries"
+                        URL                 ${CEF_DSYM_URL}
+                        UPDATE_DISCONNECTED 1)
+        set(CEF_DSYM_FILE "cefbinaries-dsym_SOURCE_DIR}/Chromium Embedded Framework.dSYM" CACHE PATH "CEF dSYM file" FORCE)
+    endif()
 endif()
 
 message(STATUS "CEF library found: target libcef_lib defined!")
