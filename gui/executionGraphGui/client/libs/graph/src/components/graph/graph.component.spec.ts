6038306e0b7e5f2ac2b7fdf8c96b99137055cdce
cmonspqr <simon.spoerri@gmail.com>
2018-07-25 11:54:28 +0200

    Make a test of the graph

--- gui/executionGraphGui/client/libs/graph/src/components/graph/graph.component.spec.ts
+++ gui/executionGraphGui/client/libs/graph/src/components/graph/graph.component.spec.ts
@@ -1,30 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, getTestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { GraphComponent } from './graph.component';
+import { PortComponent } from '../port/port.component';
+import { ConnectionComponent } from '../connection/connection.component';
+
+@Component({
+  selector: 'test',
+  template: `
+<ngcs-graph>
+  <ngcs-port id="s"></ngcs-port>
+  <ngcs-port id="t"></ngcs-port>
+  <ngcs-connection from="s" to="t"></ngcs-connection>
+</ngcs-graph>
+`})
+export class TestComponent {}
+
 
 describe('GraphComponent', () => {
-  let component: GraphComponent;
-  let fixture: ComponentFixture<GraphComponent>;
-
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
   beforeEach(
     async(() => {
       TestBed.configureTestingModule({
-        declarations: [GraphComponent]
+        declarations: [TestComponent, GraphComponent, PortComponent, ConnectionComponent]
       }).compileComponents();
     })
   );
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GraphComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
+    
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should throw when adding a connection to an inexistant port', () => {
-    expect(() => component.registerConnection('foo', 'bar')).toThrow();
+  it('should draw a line when adding a connection', () => {
+    
+    let pathElement = fixture.debugElement.query(By.css('path'));
+    expect(pathElement).toBeDefined();
+    expect(pathElement.nativeElement.attributes.d.value).toBe("M0,0 C0,0 0,0 0,0");
   })
 });
