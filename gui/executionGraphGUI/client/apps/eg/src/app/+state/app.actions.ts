decbac1bb971bcb1592dca983ee7463acbee1156
cmonspqr <simon.spoerri@gmail.com>
2018-08-24 14:56:14 +0200

    Academic excercise on CQRS

---
+++ gui/executionGraphGUI/client/apps/eg/src/app/+state/app.actions.ts
@@ -0,0 +1,54 @@
+import { Action } from '@ngrx/store';
+import { Graph } from '../model/Graph';
+import { Id } from '@eg/common/src';
+import { Socket, Connection, OutputSocket, InputSocket } from '../model';
+
+export enum AppActionTypes {
+  LoadApp = '[App] Load App',
+  AppLoaded = '[App] App Loaded',
+  AppLoadError = '[App] App Load Error',
+  SelectGraph = '[Graph] Select Graph',
+  AddConnection = '[Graph] Add Connection',
+  ConnectionAdded = '[Graph] Connection Added',
+
+}
+
+export class LoadApp implements Action {
+  readonly type = AppActionTypes.LoadApp;
+}
+
+export class AppLoadError implements Action {
+  readonly type = AppActionTypes.LoadApp;
+  constructor(public payload: any) {}
+}
+
+export class AppLoaded implements Action {
+  readonly type = AppActionTypes.AppLoaded;
+  constructor(public payload: Graph[]) {}
+}
+
+export class SelectGraph implements Action {
+  readonly type = AppActionTypes.SelectGraph;
+  constructor(public id: Id) { }
+}
+
+export class AddConnection implements Action {
+  readonly type = AppActionTypes.AddConnection;
+  constructor(public source: OutputSocket, public target: InputSocket) { }
+}
+
+export class ConnectionAdded implements Action {
+  readonly type = AppActionTypes.ConnectionAdded;
+  constructor(public connection: Connection) { }
+}
+
+export type AppAction = LoadApp | AppLoaded | AppLoadError | SelectGraph | AddConnection | ConnectionAdded;
+
+export const fromAppActions = {
+  LoadApp,
+  AppLoaded,
+  AppLoadError,
+  SelectGraph,
+  AddConnection,
+  ConnectionAdded
+};
---
+++ gui/executionGraphGUI/client/apps/eg/src/app/+state/app.effects.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Effect, Actions } from '@ngrx/effects';
+import { DataPersistence } from '@nrwl/nx';
+
+import { Id } from '@eg/common';
+import { LoggerFactory, ILogger } from '@eg/logger';
+import { AppState } from './app.reducer';
+import { LoadApp, AppLoaded, AppLoadError, AppActionTypes, ConnectionAdded, AddConnection } from './app.actions';
+import { Graph, Connection, InputSocket, SocketIndex, Node, NodeId, OutputSocket } from '../model';
+
+@Injectable()
+export class AppEffects {
+  private readonly log: ILogger;
+
+  @Effect()
+  loadApp$ = this.dataPersistence.fetch(AppActionTypes.LoadApp, {
+    run: (action: LoadApp, state: AppState) => {
+      // Your custom REST 'load' logic goes here. For now just return an empty list...
+      const n1 = new Node(
+        new NodeId(0), 
+        'Add', 
+        'Eis und eis git drüü', 
+        [new InputSocket('float', 'Lustiger Pete', new SocketIndex(0)), new InputSocket('float', 'Listiger Luch', new SocketIndex(1))],
+        [new OutputSocket('float', 'Garstiger Hans', new SocketIndex(0))]);
+
+      const n2 = new Node(
+        new NodeId(1),
+        'Const',
+        'Foif',
+        [],
+        [new OutputSocket('float', 'Dä Foifer', new SocketIndex(0))]);
+
+      const n3 = new Node(
+        new NodeId(2),
+        'Const',
+        'Sächs',
+        [],
+        [new OutputSocket('float', 'Dä Sächser', new SocketIndex(0))]);
+      
+      const g = new Graph(new Id(), [n1, n2, n3], []);
+
+      return new AppLoaded([
+       g
+      ]);
+    },
+
+    onError: (action: LoadApp, error) => {
+      console.error('Error', error);
+      return new AppLoadError(error);
+    }
+  });
+
+  @Effect()
+  addConnection$ = this.dataPersistence.pessimisticUpdate(AppActionTypes.AddConnection, {
+    run: (action: AddConnection, state: AppState) => {
+      return new ConnectionAdded(new Connection(action.source, action.target));
+    },
+
+    onError: (action: AddConnection, error) => {
+      console.error('Error', error);
+      return new AppLoadError(error);
+    }
+  });
+
+  constructor(private actions$: Actions, private dataPersistence: DataPersistence<AppState>, loggerFactory: LoggerFactory) {
+    this.log = loggerFactory.create('AppEffects');
+  }
+}
