b88e33384706da3d3b65f10fa40ec2dfeac18b04
Gabriel Nuetzi <gnuetzi@gmail.com>
2018-09-06 23:37:36 +0200

    NPM Prebuild corrections

--- gui/executionGraphGUI/client/scripts/copyFlatBuffersBuildFiles.sh
+++ gui/executionGraphGUI/client/scripts/copyFlatBuffersBuildFiles.sh
@@ -4,9 +4,9 @@ REPO_DIR="$(git rev-parse --show-toplevel)"
 
 echo "Copy flatbuffers build files to libs/serialization..."
 # copy all TS files to the client
-cp  "${REPO_DIR}/include/executionGraph/serialization/ts/*" \
+cp  "${REPO_DIR}/include/executionGraph/serialization/schemas/ts/"* \
     "${REPO_DIR}/gui/executionGraphGUI/client/libs/serialization"
 
 echo "Copy flatbuffers build files to libs/messages..."
-cp  "${REPO_DIR}/gui/executionGraphGUI/messages/schemas/ts/*" \
+cp  "${REPO_DIR}/gui/executionGraphGUI/messages/schemas/ts/"* \
     "${REPO_DIR}/gui/executionGraphGUI/client/libs/messages"
