53670b0f78a428a46f51cd9b64b349ed02d82896
Gabriel Nuetzi <gnuetzi@gmail.com>
2018-09-11 22:56:30 +0200

    Added some actions

--- gui/executionGraphGUI/client/apps/eg/src/app/+state/app.actions.ts
+++ gui/executionGraphGUI/client/apps/eg/src/app/+state/app.actions.ts
@@ -7,10 +7,21 @@ export enum AppActionTypes {
   LoadApp = '[App] Load App',
   AppLoaded = '[App] App Loaded',
   AppLoadError = '[App] App Load Error',
-  SelectGraph = '[Graph] Select Graph',
+
+  AddGraph = "[App] Add Graph",
+  GraphAdded = "[App] Graph Added",
+  RemoveGraph = "[App] Remove Graph",
+  GraphRemoved = "[App] Graph Removed",
+
+  SelectGraph = '[App] Select Graph',
+
+  AddNode = "[Graph] Add Node",
+  NodeAdded = "[Graph] Node Added",
+  RemoveNode = "[Graph] Remove Node",
+  NodeRemoved = "[Graph] Node Removed",
+
   AddConnection = '[Graph] Add Connection',
   ConnectionAdded = '[Graph] Connection Added',
-
 }
 
 export class LoadApp implements Action {
@@ -32,6 +43,50 @@ export class SelectGraph implements Action {
   constructor(public id: Id) { }
 }
 
+// Actions related to GraphManagementService
+//------------------------------------------
+export class AddGraph implements Action {
+  readonly type = AppActionTypes.AddGraph;
+  constructor(public graph: Graph) { }
+}
+
+export class GraphAdded implements Action {
+  readonly type = AppActionTypes.GraphAdded;
+  constructor(public graph: Graph) { }
+}
+
+export class RemoveGraph implements Action {
+  readonly type = AppActionTypes.RemoveGraph;
+  constructor(public id: Id) { }
+}
+
+export class GraphRemoved implements Action {
+  readonly type = AppActionTypes.GraphRemoved;
+  constructor(public id: Id) { }
+}
+
+// Actions related to GraphManipulationService
+//--------------------------------------------
+export class AddNode implements Action {
+  readonly type = AppActionTypes.AddNode;
+  constructor(public node: Node) { }
+}
+
+export class NodeAdded implements Action {
+  readonly type = AppActionTypes.NodeAdded;
+  constructor(public node: Node) { }
+}
+
+export class RemoveNode implements Action {
+  readonly type = AppActionTypes.RemoveNode;
+  constructor(public id: Id) { }
+}
+
+export class NodeRemoved implements Action {
+  readonly type = AppActionTypes.NodeRemoved;
+  constructor(public id: Id) { }
+}
+
 export class AddConnection implements Action {
   readonly type = AppActionTypes.AddConnection;
   constructor(public source: OutputSocket | InputSocket, public target: OutputSocket | InputSocket) { }
@@ -42,13 +97,16 @@ export class ConnectionAdded implements Action {
   constructor(public connection: Connection) { }
 }
 
+/**
+ *  All Actions for this application.
+ */
 export type AppAction = LoadApp | AppLoaded | AppLoadError | SelectGraph | AddConnection | ConnectionAdded;
 
 export const fromAppActions = {
   LoadApp,
   AppLoaded,
   AppLoadError,
-  SelectGraph,
+  RemoveGraph,
   AddConnection,
   ConnectionAdded
 };
