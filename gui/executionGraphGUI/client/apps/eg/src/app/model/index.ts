3cfaed53b61da5112ffbbfef3afac5b6c4ec83a1
Gabriel Nuetzi <gnuetzi@gmail.com>
2018-09-28 23:00:29 +0200

    compile fix

--- gui/executionGraphGUI/client/apps/eg/src/app/model/index.ts
+++ gui/executionGraphGUI/client/apps/eg/src/app/model/index.ts
@@ -2,7 +2,7 @@ export { Graph } from './Graph';
 export { Node, NodeId } from './Node';
 export {
   Socket,
-  SocketType
+  SocketType,
   SocketId, SocketIndex,
   InputSocket, OutputSocket,
   isOutputSocket, isInputSocket,
