11fee6d63c9e34fb78620b0e16d5daada92b39c7
Gabriel Nützi <gnuetzi@gmail.com>
2019-01-21 20:44:17 +0100

    Get rid of NodeId in LayoutEngine. Stripped down the ILayoutEngine to the bare minimum whats needed. Lint -> All Ok!

--- gui/executionGraphGui/client/libs/graph/src/lib/services/layout/ILayoutEngine.ts
+++ gui/executionGraphGui/client/libs/graph/src/lib/services/layout/ILayoutEngine.ts
@@ -9,31 +9,23 @@
 //  License, v. 2.0. If a copy of the MPL was not distributed with this
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // =========================================================================================
-import { NodeId } from 'apps/eg/src/app/model';
 import { Observable } from 'rxjs';
 import { Vector2 } from '@eg/common';
 
-export type NodeMap<Node> = Map<NodeId, Node>;
-
-export interface EngineInput<Node, Edge> {
-  nodes: Node[];
-  edges: Edge[];
-}
-
-export interface EngineOutputState {
+export interface InputState {
   pos: Vector2;
-  id: NodeId;
   opaqueData: any;
 }
-export type EngineOutput = EngineOutputState[];
+export type OutputState = InputState;
+export type EngineOutput = OutputState[];
 
-export type NodeCreator<Node> = (id: NodeId, pos: Vector2, opaqueData: any) => Node;
+export type NodeCreator<Node> = (state: InputState) => Node;
 export type EdgeCreator<Node, Edge> = (b1: Node, b2: Node) => Edge;
 
 export type GraphConverter = <Node, Edge>(
   createNode: NodeCreator<Node>,
   createEdge: EdgeCreator<Node, Edge>
-) => Promise<EngineInput<Node, Edge>>;
+) => Promise<void>;
 
 export abstract class ILayoutEngine {
   public abstract run(converter: GraphConverter): Observable<EngineOutput>;
