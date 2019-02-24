f486c93e9049d217b87e1011f21679d7a1248745
Gabriel Nützi <gnuetzi@gmail.com>
2019-02-24 15:13:18 +0100

    Build stuff.

---
+++ gui/executionGraphGui/client/cmake/package.json.cmake
@@ -0,0 +1,34 @@
+{
+  "name": "eg",
+  "version": "1.0.0",
+  "license": "MPL 2.0",
+  "author": "Gabriel Nützi, Simon Spörri",
+  "description": "Build and execute data flow graphs",
+  "private": true,
+  "build": {
+    "appId": "executionGraphGui",
+    "productName": "ExecutionGraphGui",
+    "copyright": "Copyright © 2018 ${author}",
+    "directories": {
+      "output": "package"
+    },
+    "files": [
+      "build"
+    ],
+    "extraResources": [],
+    "linux": {
+      "target": "tar.gz",
+      "icon": "@ExecutionGraph_CLIENT_DIR@/apps/electron/resources/icon.png"
+    },
+    "mac": {
+      "target": "dmg",
+      "category": "com.${name}.app",
+      "icon": "@ExecutionGraph_CLIENT_DIR@/apps/electron/resources/icon.png"
+    },
+    "win": {
+      "target": "zip",
+      "icon": "@ExecutionGraph_CLIENT_DIR@/apps/electron/resources/icon.png"
+    },
+  }
+  "main": "build/main.js"
+}
